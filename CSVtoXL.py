82b20b0e978317f280434d4c38c2a03813a2fa1f
Gary Lee <leegaryhaig@gmail.com>
2018-11-27 12:29:46 -0800

    Working Version

---
+++ CSVtoXL.py
@@ -0,0 +1,40 @@
+import openpyxl, csv, os
+from openpyxl.utils import get_column_letter
+
+# Creates wb object
+wb = openpyxl.Workbook()
+
+def sheetLimit
+
+def convertCSV(name):
+    # Loop through each row and column of the csvData array in the corresponding excel sheet
+    sheetname = wb.create_sheet(name)
+    for row_num in range(len(csvData)):
+        for col_num in range(len(csvData[row_num]) - 1):
+            # print('sheetname.cell(' + str(get_column_letter(col_num + 1)) + str(row_num + 1) + ')' )
+            # print('sheetname.cell(' + str(get_column_letter(col_num + 1)) + str(row_num + 1) + ')' + csvData[row_num][col_num])
+            sheetname.cell(row=row_num + 1, column=col_num + 1).value = csvData[row_num][col_num]
+
+curDir = []
+for files in os.listdir('.'):
+    curDir.append(files)
+
+if 'csvfiles' in curDir:
+    for root, dirs, files in os.walk('csvfile'):
+        for name in files:
+            if name.endswith('.csv'):
+                abs_path = os.path.join(root, name)
+                sheetname = name.rstrip('.csv')
+                csvFile = open(abs_path)
+                csvReader = csv.reader(csvFile)
+                csvData = list(csvReader)
+                convertCSV(name)
+
+    xl_filename = input('Save Excel File as:\n>')
+    csvFile.close()
+    wb.remove_sheet('Sheet')
+    wb.save(str(xl_filename) + '.xlsx')
+    wb.close()
+
+else:
+    print('can\'t find the csvfiles folder')
